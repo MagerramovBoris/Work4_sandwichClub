83dc57ee7abf7ef969096f7819ced7188ad4864a
MagerramovBoris <magerramov.boris.s@gmail.com>
2021-01-24 23:16:00 +0700

    Пункт 7. Добавил RecyclerView. Обновил Gradle для того, чтобы RV заработал. Исправил ошибки. Проверил проект на работоспособность.

--- app/src/main/java/com/udacity/Work4_sandwichClub/DetailActivity.java
+++ app/src/main/java/com/udacity/Work4_sandwichClub/DetailActivity.java
@@ -34,13 +34,10 @@ public class DetailActivity extends AppCompatActivity {
         mDescriptionTv = findViewById(R.id.description_tv);
         mIngredientsTv = findViewById(R.id.ingredients_tv);
         mAlsoKnownAsTv = findViewById(R.id.also_known_tv);
-
-
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
-
         // Параметры, полученные при нажатии на элемент с списке
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
@@ -48,53 +45,41 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
             return;
         }
-
         // Получение массива sandwiches из строк, где каждая строка
         // соответствует отдельному сэндвичу
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-
         // Получение одного сэндвича по его позиции
         String json = sandwiches[position];
-
         // Полученный sandwich десеаризируем и превращаем в объект
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
-
         // Заполняем контентом
         populateUI(sandwich);
     }
-
     // Применяется, если сэндвич не найден
     private void closeOnError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
-
     // Заполнение контентом
     private void populateUI(Sandwich sandwich) {
         // Название в header
         setTitle(sandwich.getMainName());
         Picasso.with(this)
                 .load(sandwich.getImage()).into(ingredientsIv);
-
         mPlaceOfOriginTv.setText(sandwich.getPlaceOfOrigin());
-
         mDescriptionTv.setText(sandwich.getDescription());
-
         // Преобразование массива в строку, где элементы массива разделены запятой
         String formatTextIngredients = android.text.TextUtils.join(", ",
                 sandwich.getIngredients());
         mIngredientsTv.setText(formatTextIngredients);
-
         // Преобразование массива в строку, где элементы массива разделены запятой
         String formatTextAlsoKnownAs = android.text.TextUtils.join(", ",
                 sandwich.getAlsoKnownAs());
-
         mAlsoKnownAsTv.setText(formatTextAlsoKnownAs);
     }
 }
--- app/src/main/java/com/udacity/Work4_sandwichClub/MainActivity.java
+++ app/src/main/java/com/udacity/Work4_sandwichClub/MainActivity.java
@@ -1,38 +1,90 @@
 package com.udacity.Work4_sandwichClub;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Массив сэндвичей
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, sandwiches);
-
-        // Simplification: Using a ListView instead of a RecyclerView
-        ListView listView = findViewById(R.id.sandwiches_listview);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                launchDetailActivity(position);
-            }
-        });
+        // Получить RV
+        View recyclerView = findViewById(R.id.sandwiches_list);
+        assert recyclerView != null;
+        //  Задать адаптер в RV
+        setupRecyclerView((RecyclerView) recyclerView, Arrays.asList(sandwiches));
     }
+    // Установка кастомного адаптера для RV
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView, List<String> sandwiches) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, sandwiches));
+    }
+    // Адаптер для RV
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+        private final MainActivity mParentActivity;
+        private final List mValues;
 
-    private void launchDetailActivity(int position) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_POSITION, position);
-        startActivity(intent);
+        // Конструктор
+        SimpleItemRecyclerViewAdapter(MainActivity parent, List<String> items) {
+            mValues = items;
+            mParentActivity = parent;
+        }
+        // Слушатели кликов по элементам списка
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Получение номера бутерброда из тега элемента
+                int position = (int) view.getTag();
+                // Получаем контектс
+                Context context = view.getContext();
+                Intent intent = new Intent(context, DetailActivity.class);
+                // Передача параметров  (номер бутерброда)
+                intent.putExtra(DetailActivity.EXTRA_POSITION, position);
+                // Запускаем Activity с описание бутерброда
+                context.startActivity(intent);
+            }
+        };
+        // возвращает объект ViewHolder
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item, parent, false);
+            return new ViewHolder(view);
+        }
+        // выполняет привязку объекта ViewHolder к объекту по определенной позиции
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            // название бутерброда в поле tv_name каждого элемента списка
+            holder.mIdView.setText( String.valueOf(mValues.get(position)));
+            // Номер элемента в тег элемента списка
+            holder.itemView.setTag(position);
+            // Навешиваем слушатель
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+        // возвращает количество объектов в списке
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+        // хранит данные по одному объекту
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            ViewHolder(View view) {
+                super(view);
+                mIdView = (TextView) view.findViewById(R.id.tv_name);
+            }
+        }
     }
 }
